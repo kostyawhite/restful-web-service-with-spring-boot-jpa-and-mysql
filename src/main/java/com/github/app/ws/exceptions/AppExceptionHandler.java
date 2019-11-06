56f2d2bb5ac256cf8c581856f51e0316dad44757
kostyawhite <kostyawhite89@gmail.com>
2019-11-06 16:04:57 +0300

    61. Handle a Specific Exception

---
+++ src/main/java/com/github/app/ws/exceptions/AppExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.github.app.ws.exceptions;
+
+import com.github.app.ws.service.UserService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+    @ExceptionHandler(value = { UserServiceException.class })
+    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
+        return new ResponseEntity<>(ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
