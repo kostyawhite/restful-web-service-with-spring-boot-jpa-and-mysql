1ffdb414d5c44e17a5bb812f85ddf6f7c2731b7f
kostyawhite <kostyawhite89@gmail.com>
2019-11-06 16:20:01 +0300

    62. Return Custom Error Object Representation

--- src/main/java/com/github/app/ws/exceptions/AppExceptionHandler.java
+++ src/main/java/com/github/app/ws/exceptions/AppExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.github.app.ws.exceptions;
 
 import com.github.app.ws.service.UserService;
+import com.github.app.ws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +9,18 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionHandler {
 
     @ExceptionHandler(value = { UserServiceException.class })
     public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
-        return new ResponseEntity<>(ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
